4f88441676c55c81ac26bf75d47bbf7d0a9f3600
Drakepal <playonplay23@gmail.com>
2023-01-17 13:25:37 -0600

    activity_splash is done

--- app/src/main/java/com/example/recipe/View/SplashActivity.kt
+++ app/src/main/java/com/example/recipe/View/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.example.recipe
+package com.example.recipe.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recipe.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
