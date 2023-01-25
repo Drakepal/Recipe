f4332974a79fdfebb808bf52b5b9abe3c3724722
Drakepal <playonplay23@gmail.com>
2023-01-25 14:43:23 -0600

    get started button works

--- app/src/main/java/com/example/recipe/View/SplashActivity.kt
+++ app/src/main/java/com/example/recipe/View/SplashActivity.kt
@@ -1,12 +1,21 @@
 package com.example.recipe.View
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.recipe.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
+        btnGetStarted.setOnClickListener {
+            var intent = Intent(this@SplashActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
