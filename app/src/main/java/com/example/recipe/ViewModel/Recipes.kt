6c15f5f778022523377d5d34cd9fce3d081de245
Drakepal <playonplay23@gmail.com>
2023-01-26 12:31:32 -0600

    added the recipes file in the ViewModel folder

---
+++ app/src/main/java/com/example/recipe/ViewModel/Recipes.kt
@@ -0,0 +1,17 @@
+package com.example.recipe.ViewModel
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+
+@Entity(tableName = "Recipes")
+data class Recipes (
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+
+    @ColumnInfo(name = "dishname")
+    var  dishname: String
+
+) : Serializable
